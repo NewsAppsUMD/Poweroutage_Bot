6aa16e28b6756500ee2b295bef84a854c47e7362
Fatema Hosseini <113056841+helia786@users.noreply.github.com>
2023-04-08 17:46:01 +0000

    let's check now...

--- scrapers/poweroutages.py
+++ scrapers/poweroutages.py
@@ -38,7 +38,7 @@ if response.status_code == 200:
 else:
     print("Failed to retrieve data from API")
 
-slack_api_token = os.environ.get('SLACK_API_TOKEN')
+slack_api_token = os.environ.get('slack_api_token')
 
 client = WebClient(token=slack_api_token)
 
